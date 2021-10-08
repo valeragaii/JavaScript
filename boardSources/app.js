6e74ebc56a90dba9af2d6e578e306860ea56f42a
valeravangaii <valeriavangai@gmail.com>
2021-10-08 10:55:02 +0200

    Examples of quick decisions on JS

---
+++ boardSources/app.js
@@ -0,0 +1,30 @@
+const board = document.querySelector('#board')
+const colors = ['#B83AEF', '#FE2079', '#409CF7', '#2FFA2F', '#F4F459']
+const SQUARES_NUMBER = 253
+
+for (let i = 0; i < SQUARES_NUMBER; i++) {
+    const square = document.createElement('div')
+    square.classList.add('square')
+
+    square.addEventListener('mouseover', () => setColor(square))
+
+    square.addEventListener('mouseleave', () => removeColor(square))
+
+    board.append(square)
+}
+
+function setColor(element) {
+    const color = getRandomColor()
+    element.style.backgroundColor = color
+    element.style.boxShadow = `0 0 2px ${color}, 0 0 10px ${color}`
+}
+
+function removeColor(element) {
+    element.style.backgroundColor = '#1d1d1d'
+    element.style.boxShadow = `0 0 2px #000`
+}
+
+function getRandomColor() {
+ const index = Math.floor(Math.random() * colors.length)
+ return colors[index]
+}
